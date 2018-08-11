6eb8da5a1e75e5186acbd6ccb79b9cf4e0a3f48b
Hengyu Lou <hylou34@gmail.com>
2018-08-11 16:29:49 -0700

    Connect frontend server to backend server

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,13 @@ const passport = require('passport');
 
 module.exports = app => {
   app.get(
+    '/',  
+    (req, res) => {
+      res.send("Comming soon...");
+    }
+  );
+
+  app.get(
     '/auth/google',
     passport.authenticate('google', {
       scope: ['profile', 'email']
